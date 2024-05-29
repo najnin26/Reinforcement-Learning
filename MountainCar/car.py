69c35ee9bae6f0251d2ba05d9ed3554c3d94aee1
Najnin Sultana Shirin <najninshirin26@gmail.com>
2024-05-29 21:55:40 +0600

    Update car.py

--- MountainCar/car.py
+++ MountainCar/car.py
@@ -3,13 +3,13 @@ import gymnasium as gym
 env = gym.make("MountainCar-v0", render_mode='human')
 state = env.reset()
 
-print(env.observation_space.high)
-print(env.observation_space.low)
+# print(env.observation_space.high)
+# print(env.observation_space.low)
 
-# done = False
-# while not done:
-#     action = 2
-#     new_state, reward, done, truncated,info = env.step(action)
-#     env.render()
+done = False
+while not done:
+    action = 2
+    new_state, reward, done, truncated,info = env.step(action)
+    env.render()
     
-# env.close()
+env.close()
